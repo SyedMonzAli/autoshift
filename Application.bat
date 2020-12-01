aee260bc5f2f8c8d28f7f03987c9077f5d7724b8
SyedMonzAli <47460937+SyedMonzAli@users.noreply.github.com>
2020-12-01 23:02:15 +0000

    Add files via upload

---
+++ Application.bat
@@ -0,0 +1,2 @@
+@echo off
+java --module-path javafx-sdk-13.0.2\lib --add-modules=javafx.controls,javafx.fxml -jar dist\AutoShiftScheduler.jar
---
+++ Run.vbs
@@ -0,0 +1,2 @@
+'HideBat.vbs
+CreateObject("Wscript.Shell").Run "Application.bat", 0, True
